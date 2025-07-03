59be9e94cbf8f749158f3524bbcd86ec3cfdc69a
Your Name <you@example.com>
2025-07-03 21:17:27 +0200

    Listen on 0.0.0.0 for Render

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const { Pool } = require('pg');
 const app = express();
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 app.get('/', async (req, res) => {
