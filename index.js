b4e4fec11265f7e2c869663ca3786b85d65658fe
Your Name <you@example.com>
2025-07-03 21:01:11 +0200

    Listen on 0.0.0.0 for Render

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.get('/', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
+
